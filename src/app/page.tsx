8a036531b945a0f6c2722ccee699bf207223bb2e
Victor Eykel <victoreykel@gmail.com>
2025-06-10 05:09:32 -0300

    Alteração Teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1 className="text-3xl sm:text-4xl font-semibold tracking-tight text-center sm:text-left">
+          Começo
+        </h1>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
